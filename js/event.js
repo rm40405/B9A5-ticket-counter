2a1edff8ed60d5c82e1e55a6cef60c6f78a7afe2
rm40405 <rm404085@gmail.com>
2024-09-22 14:39:38 +0600

    ticket counter project complete

---
+++ js/event.js
@@ -0,0 +1,17 @@
+// const showCuponPriceEl = document.getElementById('show-cupon-price');
+//     showCuponPriceEl.innerHTML = `
+//     <p>Discount</p>
+//     <ul class="flex justify-between py-3">
+              
+//     <p>BDT:
+//          <span>-BDT</span>
+//          <span>${discountPriceEl.toFixed(2)}</span>
+//                 </p>
+//             </ul>
+//     `;
+//     const discountPriceEl = totalPrice - grandPrice;
+    
+//     const toalGrandValue =  grandPrice
+//     totalGrande.innerText = toalGrandValue.toFixed(2);
+// })
+    
--- js/ticket.js
+++ js/ticket.js
@@ -1,11 +1,92 @@
 // function seat handleing
 const selectSeat =document.getElementById('selected-seat');
-
+const totalSeat = document.getElementById('total-seat');
+const totalPZ = document.getElementById('total-price');
+const cuponField = document.getElementById('cupon-field');
+const cuponBtn = document.getElementById('cupon-btn');
+const totalGrande = document.getElementById('total-grand-price');
+const defaultSeat = document.getElementById('default-seat');
+const nextBtn = document.getElementById('next-btn');
+const phoneNumber = document.getElementById('phone-number');
+let array =[];
+let totalPrice = 0;
 function handleBySeat(event){
-    console.log(event.innerText);
-    selectSeat.innerHTML = `<li class="flex justify-between">
+    const value = event.innerText
+    if(array.includes(value)){
+        return alert('seat already selected');
+    }
+   else if(array.length < 4){
+    event.classList.add('bg-primary');
+     event.classList.add('text-white')
+     array.push(event.innerText);
+     totalSeat.innerText = array.length;
+    //  default seat
+      defaultSeat.classList.add('hidden');
+    //  select seat 
+    selectSeat.innerHTML += `<li class="flex justify-between">
     <span>${event.innerText}</span>
     <span>economy</span>
     <span>550</span>
-    </li>`
+    </li>`;
+    // total seat count
+    totalPrice += 550;
+    totalPZ.innerText = totalPrice;
+   }
+   else{
+    return alert('only 4 seats can be selected');
+   }
+     
+
+     if(array.length > 3){
+        cuponField.removeAttribute('disabled');
+        cuponBtn.removeAttribute('disabled');
+     }
+    
+    
+
 }
+
+// Grand total price
+
+document.getElementById('cupon-btn').addEventListener('click', function(){
+    const cuponFieldValue =cuponField.value ;
+    let grandTotalPrice = 0;
+    if(cuponFieldValue !== 'NEW50' && uponFieldValue !== 'NEW10'  ){
+        alert('please select a coupon');
+        return;
+    }
+    if(cuponFieldValue === 'NEW50'){
+      grandTotalPrice = totalPrice*.15;
+    }
+    else if(cuponFieldValue === 'NEW10'){
+        grandTotalPrice = totalPrice*.05;
+    }
+    const grandTotalValue = totalPrice - grandTotalPrice
+    totalGrande.innerText = grandTotalValue
+    // show discount price
+    const showCuponPriceEl = document.getElementById('show-cupon-price');
+    showCuponPriceEl.innerHTML = `
+   
+    
+  <p>Discount</p>
+    
+     <p>
+         <span>-BDT</span>
+         <span>${grandTotalPrice.toLocaleString(2)}</span>
+    </p>
+ 
+    `;
+    
+    });
+    // enable
+    phoneNumber.addEventListener('input', function(e){
+     const phoneValue = e.target.value;
+     console.log(phoneValue);
+     if(phoneValue.length >= 11){
+        nextBtn.removeAttribute('disabled');
+     }
+    })
+    document.getElementById('btn-continue').addEventListener('click',function(){
+        window.location.reload();
+        
+    })
